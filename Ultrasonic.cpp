967c60b02642ecd909a3747118341f87bbe64e37
Liam Kinne <kinneliam@gmail.com>
2017-07-14 22:04:16 +1000

    Changed control flow to have min time between pulses

--- Ultrasonic.cpp
+++ Ultrasonic.cpp
@@ -8,14 +8,20 @@ Ultrasonic::Ultrasonic(uint8_t trigPin, uint8_t echoPin): trigPin(trigPin), echo
 }
 
 uint16_t Ultrasonic::get(void) {
-	// Pulse trigger pin
-	Utils::pulse(this->trigPin, this->trigPulseDelta, HIGH);
-	// Wait unil signal goes high
-	while(!digitalRead(this->echoPin));
-	// Remember start time
-	uint32_t startTime = micros();
-	// Wait while signal is high
-	while(digitalRead(this->echoPin));
-	// Return calculated value in mm
-	return (micros() - startTime) / 5.8f;
+	return distance;
+}
+
+void Ultrasonic::refresh(void) {
+	if (millis() - lastTrig >= trigMinDelta) {
+		// Pulse trigger pin
+		Utils::pulse(this->trigPin, this->trigPulseDelta, HIGH);
+		// Wait unil signal goes high
+		while(!digitalRead(this->echoPin));
+		// Remember start time
+		uint32_t startTime = micros();
+		// Wait while signal is high
+		while(digitalRead(this->echoPin));
+		// Return calculated value in mm
+		this->distance = (micros() - startTime) / 5.8f;
+	}
 }
--- Ultrasonic.h
+++ Ultrasonic.h
@@ -11,17 +11,18 @@ namespace Phantom {
 		uint8_t echoPin;
 		uint16_t trigPulseDelta = 10; // In Milliseconds
 
-		//uint16_t distance;
+		uint16_t distance;
 
 		// Timing
-		//uint16_t lastTrig;
+		uint16_t lastTrig;
+		uint16_t trigMinDelta = 100; // Min time between triggering the sensor
 
 	public:
 		Ultrasonic(uint8_t trigPin, uint8_t echoPin);
 
 		uint16_t get(void); // returns last value aquired in mm
 
-		//void refresh(); // Needed when making non blocking
+		void refresh(void); // Needed when making non blocking
 	};
 }
 
