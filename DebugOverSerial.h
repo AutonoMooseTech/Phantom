df2c16ed4d1f5c2b6f614f5eab02809b2993747d
Liam Kinne <kinneliam@gmail.com>
2017-07-17 00:01:21 +1000

    Debugging thing

---
+++ DebugOverSerial.h
@@ -0,0 +1,25 @@
+#ifndef DEBUGOVERSERIAL_H
+#define DEBUGOVERSERIAL_H
+
+#include <Arduino.h>
+#include "Utils.h"
+
+class DebugOverSerial
+{
+public:
+	DebugOverSerial(uint16_t baud);
+	
+	// Control
+	void enable(void);
+	void disable(void);
+
+	// Display
+	void print(String input);
+	void println(String input); // Automatic line ending
+	void printTitle(String input);
+private:
+	uint16_t baud;
+	bool enabled = true;
+};
+
+#endif
