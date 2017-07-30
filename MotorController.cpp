4d4f6d4bf794be742becd4cd40d112e220afbbef
liamkinne <kinneliam@gmail.com>
2017-07-31 08:26:18 +1000

    Added enable/disable functionality

--- MotorController.cpp
+++ MotorController.cpp
@@ -6,6 +6,14 @@ MotorController::MotorController(uint8_t pinPWM, uint8_t pinDirA, uint8_t pinDir
 	pinMode(this->pinDirB, OUTPUT);
 }
 
+MotorController::MotorController(uint8_t pinPWM, uint8_t pinDirA, uint8_t pinDirB, uint8_t pinEnable): pinPWM(pinPWM), pinDirA(pinDirA), pinDirB(pinDirB), pinEnable(pinEnable) {
+	pinMode(this->pinPWM, OUTPUT);
+	pinMode(this->pinDirA, OUTPUT);
+	pinMode(this->pinDirB, OUTPUT);
+
+	this->enablePin = true;
+}
+
 void MotorController::setValue(float value) {
 	this->value = value;
 }
@@ -14,8 +22,30 @@ float MotorController::getValue() {
 	return this->value;
 }
 
+void MotorController::enable() {
+	enabled = true;
+	if (enablePin) { // Check if motor controller has an enable pin
+		digitalWrite(pinEnable, HIGH);
+	}
+	else {
+		digitalWrite(pinPWM, HIGH);
+	}
+}
+
+void MotorController::disable() {
+	enabled = false;
+	if (enablePin) { // Check if motor controller has an enable pin
+		digitalWrite(pinEnable, LOW);
+	}
+	else {
+		digitalWrite(pinPWM, LOW);
+	}
+}
+
 void MotorController::update() {
-	digitalWrite(this->pinDirA, this->value > 0.0f); // Use sign to set direction pins 
-	digitalWrite(this->pinDirA, this->value < 0.0f);
-	analogWrite(this->pinPWM, Utils::mapVal(this->value, -1.0f, 1.0f, 0, 255)); // Write PWM Value
+	if (enabled) {
+		digitalWrite(pinDirA, value > 0.0f); // Use sign to set direction pins 
+		digitalWrite(pinDirA, value < 0.0f);
+		analogWrite(pinPWM, Utils::mapVal(value, -1.0f, 1.0f, 0.0f, 255.0f)); // Write PWM Value
+	}
 }
--- MotorController.h
+++ MotorController.h
@@ -7,14 +7,20 @@
 class MotorController {
 public:
 	MotorController(uint8_t pinPWM, uint8_t pinDirA, uint8_t pinDirB);
+	MotorController(uint8_t pinPWM, uint8_t pinDirA, uint8_t pinDirB, uint8_t pinEnable);
 
 	void setValue(float value);
 	float getValue();
 
+	void enable();
+	void disable();
+
 	void update();
 private:
-	uint8_t pinPWM, pinDirA, pinDirB; // Pinout
+	uint8_t pinPWM, pinDirA, pinDirB, pinEnable; // Pinout
 	float value;
+	bool enabled = true;
+	bool enablePin = false;
 };
 
 #endif
