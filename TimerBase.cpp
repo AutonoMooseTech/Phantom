0690dc721e995f3cf9ebefce0eb1b56b33a0adb1
liamkinne <kinneliam@gmail.com>
2017-08-01 04:43:27 +1000

    Timer Base Class

---
+++ TimerBase.cpp
@@ -0,0 +1,21 @@
+#include "TimerBase.h"
+
+using namespace Phantom;
+
+TimerBase::TimerBase(uint32_t interval = 100): prevTime(0), updateTime(interval) {
+	// Nothing to see here.
+}
+
+void TimerBase::force() {
+	prevTime = millis();
+	doWork();
+}
+
+void TimerBase::update() {
+	// Sentinel
+	currTime = millis();
+	if(currTime - prevTime < updateTime) return;
+	prevTime = currTime;
+
+	doWork();
+}
---
+++ TimerBase.h
@@ -0,0 +1,26 @@
+/*
+Originally authored by Simon Hogwood.
+*/
+
+#ifndef TIMERBASE_H
+#define TIMERBASE_H
+
+#include <Arduino.h>
+
+namespace Phantom {
+	class TimerBase {
+	private:
+		uint32_t currTime, prevTime, updateTime;
+
+	public:
+		TimerBase(uint32_t interval = 100);
+
+		void force();
+
+		void update(void);
+
+		virtual void doWork(void) = 0;
+	};
+}
+
+#endif
