b560961ea863f7596e7f78bc55e57d0d068ef80f
liamkinne <kinneliam@gmail.com>
2017-08-02 14:19:16 +1000

    Solenoid Controller Class

---
+++ SolenoidController.cpp
@@ -0,0 +1,33 @@
+#include "SolenoidController.h"
+
+using namespace Phantom;
+
+SolenoidController::SolenoidController(uint8_t pinTrigger, uint8_t pinVoltSense): pinTrigger(pinTrigger), pinVoltSense(pinVoltSense) {
+	pinMode(pinTrigger, OUTPUT);
+	pinMode(pinVoltSense, INPUT);
+}
+
+void SolenoidController::trigger() {
+	state = state_t::ON;
+}
+
+void SolenoidController::forceOff() {
+	state = state_t::OFF;
+}
+
+void SolenoidController::setDuration(uint16_t time) {
+	duration = time;
+}
+
+void SolenoidController::update() {
+	if (state == state_t::ON) {
+		if (stateLast == state_t::OFF) { // state changed from off to on
+			timeOfTrigger = millis();
+			digitalWrite(pinTrigger, HIGH);
+		}
+		else if (millis() - timeOfTrigger >= duration) { // state is on, but over timeout duration
+			state = state_t::OFF;
+			digitalWrite(pinTrigger, LOW);
+		}
+	}
+}
---
+++ SolenoidController.h
@@ -0,0 +1,27 @@
+#ifndef SOLENOIDCONTROLLER_H
+#define SOLENOIDCONTROLLER_H
+
+#include <Arduino.h>
+
+namespace Phantom {
+	class SolenoidController {
+	public:
+		SolenoidController(uint8_t pinTrigger, uint8_t pinVoltSense);
+		void trigger();
+
+		void setDuration(uint16_t time);
+
+		void update();
+	private:
+		uint8_t pinTrigger, pinVoltSense;
+		uint16_t duration;
+
+		enum state_t {OFF, ON};
+		state_t state = state_t::OFF;
+		state_t stateLast = state_t::OFF;
+
+		uint32_t timeOfTrigger;
+	};
+}
+
+#endif
