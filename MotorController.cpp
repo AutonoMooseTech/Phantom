bc403eeaf5c5d3157054dce59f21a2dbc1cf7986
liamkinne <kinneliam@gmail.com>
2017-08-04 12:14:20 +1000

    Simplified disable function

--- MotorController.cpp
+++ MotorController.cpp
@@ -18,36 +18,22 @@ MotorController::MotorController(uint8_t pinPWM, uint8_t pinDirA, uint8_t pinDir
 
 void MotorController::setValue(float value) {
 	this->value = value;
+	enabled = true;
 }
 
 float MotorController::getValue() {
 	return this->value;
 }
 
-void MotorController::enable() {
-	enabled = true;
-	if (enablePin) { // Check if motor controller has an enable pin
-		digitalWrite(pinEnable, HIGH);
-	}
-	else {
-		digitalWrite(pinPWM, HIGH);
-	}
-}
-
 void MotorController::disable() {
 	enabled = false;
-	if (enablePin) { // Check if motor controller has an enable pin
-		digitalWrite(pinEnable, LOW);
-	}
-	else {
-		digitalWrite(pinPWM, LOW);
-	}
+	update();
 }
 
 void MotorController::update() {
 	if (enabled) {
 		digitalWrite(pinDirA, value > 0.0f); // Use sign to set direction pins 
 		digitalWrite(pinDirA, value < 0.0f);
-		analogWrite(pinPWM, Utils::mapVal(value, -1.0f, 1.0f, 0.0f, 255.0f)); // Write PWM Value
+		analogWrite(pinPWM, Utils::mapVal(value, -1.0f, 1.0f, 0, 255)); // Write PWM Value
 	}
 }
--- MotorController.h
+++ MotorController.h
@@ -8,20 +8,17 @@ namespace Phantom {
 	class MotorController {
 	public:
 		MotorController(uint8_t pinPWM, uint8_t pinDirA, uint8_t pinDirB);
-		MotorController(uint8_t pinPWM, uint8_t pinDirA, uint8_t pinDirB, uint8_t pinEnable);
 
 		void setValue(float value);
 		float getValue();
 
 		void enable();
-		void disable();
 
 		void update();
 	private:
 		uint8_t pinPWM, pinDirA, pinDirB, pinEnable; // Pinout
 		float value = 0.0f;
 		bool enabled = true;
-		bool enablePin = false;
 	};
 }
 
