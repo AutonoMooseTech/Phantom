45b39e35ab0dd3c319013d0f002d8ccd785f4cd6
liamkinne <kinneliam@gmail.com>
2017-08-04 14:53:36 +1000

    Removed enable pin param

--- VNH2SP30.cpp
+++ VNH2SP30.cpp
@@ -5,7 +5,3 @@ using namespace Phantom;
 VNH2SP30::VNH2SP30(uint8_t pinPWM, uint8_t pinDirA, uint8_t pinDirB): MotorController(pinPWM, pinDirA, pinDirB) {
 
 }
-
-VNH2SP30::VNH2SP30(uint8_t pinPWM, uint8_t pinDirA, uint8_t pinDirB, uint8_t pinEnable): MotorController(pinPWM, pinDirA, pinDirB), pinEnable(pinEnable) {
-	
-}
--- VNH2SP30.h
+++ VNH2SP30.h
@@ -9,9 +9,7 @@ namespace Phantom {
 	class VNH2SP30: public MotorController {
 	public:
 		VNH2SP30(uint8_t pinPWM, uint8_t pinDirA, uint8_t pinDirB);
-		VNH2SP30(uint8_t pinPWM, uint8_t pinDirA, uint8_t pinDirB, uint8_t pinEnable);
 	private:
-		uint8_t pinEnable;
 	};
 }
 
