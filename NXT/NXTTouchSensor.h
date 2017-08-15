dfb39c06f1e66384bf7cf779755050d8bb1b5896
liamkinne <kinneliam@gmail.com>
2017-08-15 10:32:07 +1000

    Added namespace

--- NXT/NXTTouchSensor.h
+++ NXT/NXTTouchSensor.h
@@ -6,18 +6,20 @@
 #include "../Base.h"
 #include "../Scheduler.h"
 
-class NXTTouchSensor: virtual public Base, virtual public Scheduler {
-public:
-	NXTTouchSensor(uint8_t pinInput);
-	
-	void get();
+namespace Phantom {
+	class NXTTouchSensor: virtual public Base, virtual public Scheduler {
+	public:
+		NXTTouchSensor(uint8_t pinInput);
+		
+		void get();
 
-	void update();
+		void update();
 
-private:
-	uint8_t pinInput;
-	bool state;
-};
+	private:
+		uint8_t pinInput;
+		bool state;
+	};
+}
 
 
 #endif
