1ee1dbce8aba139112de9bf4e18d67a7ce5088a5
Liam Kinne <liamkinne@users.noreply.github.com>
2017-09-05 22:58:04 +1000

    Added encoder class

---
+++ Encoder.cpp
@@ -0,0 +1,41 @@
+#include "Encoder.h"
+
+using namespace Phantom;
+
+// 0,  1, 2, 3, 4, 5, 6,  7,  8, 9, 10, 11, 12, 13, 14, 15
+// 0, -1, 1, 0, 1, 0, 0, -1, -1, 0,  0,  1,  0,  1, -1,  0
+
+Encoder::Encoder(uint8_t pinA, uint8_t pinB): pinA(pinA), pinB(pinB) {
+	pinMode(pinA, INPUT);
+	pinMode(pinB, INPUT);
+}
+
+int32_t Encoder::get() {
+	return value;
+}
+
+int8_t Encoder::getDirection() {
+	return direction;
+}
+
+void Encoder::reset() {
+	value = 0;
+	direction = direction_t::STOPPED;
+}
+
+void Encoder::update() {
+	uint8_t state = digitalRead(pinA) | (digitalRead(pinB) << 1);
+	if (state != lastState) {
+		switch (state | (lastState << 2)) {
+			case 1: case 7: case 8:	case 14:
+				value--;
+				direction = direction_t::BACKWARDS;
+				break;
+			case 2:	case 4:	case 11: case 13:
+				value++;
+				direction = direction_t::BACKWARDS;
+				break;
+		}
+		lastState = state;
+	}
+} 
---
+++ Encoder.h
@@ -0,0 +1,35 @@
+#ifndef ENCODER_H
+#define ENCODER_H
+
+#include "Phantom.h"
+
+namespace Phantom {
+	class Encoder {
+	public:
+		enum direction_t {
+			FORWARD 	= 1,
+			STOPPED 	= 0,
+			BACKWARDS 	= -1
+		};
+		Encoder(uint8_t pinA, uint8_t pinB);
+
+		int32_t get();
+
+		int8_t getDirection();
+
+		void reset();
+
+		void update();
+		
+	private:
+		uint8_t pinA;
+		uint8_t pinB;
+		int32_t value;
+		direction_t direction;
+
+		uint8_t lastState;
+		
+	};
+}
+
+#endif
