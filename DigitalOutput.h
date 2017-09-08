8eb3b18e1d3f021c3caa108ad8b9eb7da8ccc941
Liam Kinne <liamkinne@users.noreply.github.com>
2017-09-08 14:37:52 +1000

    Removed unused enumerator

--- DigitalOutput.h
+++ DigitalOutput.h
@@ -6,15 +6,9 @@
 namespace Phantom {
 	class DigitalOutput {
 	public:
-		enum state_t {
-			LOW,
-			HIGH
-		};
-
 		DigitalOutput(uint8_t pin);
 		
 		void set(bool state);
-		void set(state_t state);
 	private:
 		uint8_t pin;
 	};
