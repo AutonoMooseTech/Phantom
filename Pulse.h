ad8fb31cba89c274f324c67805f92f6bbdc46410
Liam Kinne <liamkinne@users.noreply.github.com>
2017-09-08 14:38:04 +1000

    Updated to use digital output class

--- Pulse.h
+++ Pulse.h
@@ -2,38 +2,47 @@
 #define PULSE_H
 
 #include "Phantom.h"
+#include "DigitalOutput.h"
 
 namespace Phantom {
 	// Pulse and output HIGH and then return to LOW after given delay (in milliseconds)
 	// Note this function is blocking
 	inline void pulseHigh(byte pin, uint32_t deltaUs = 0) {
-		digitalWrite(pin, HIGH);
+		auto output = new DigitalOutput(pin);
+		output->set(HIGH);
 		if(deltaUs) delay(deltaUs);
-		digitalWrite(pin, LOW);
+		output->set(LOW);
+		delete output;
 	}
 
 	// Pulse and output HIGH and then return to LOW after given delay (in microseconds)
 	// Note this function is blocking
 	inline void pulseHighMicro(byte pin, uint32_t deltaUs = 0) {
-		digitalWrite(pin, HIGH);
+		auto output = new DigitalOutput(pin);
+		output->set(HIGH);
 		if(deltaUs) delayMicroseconds(deltaUs);
-		digitalWrite(pin, LOW);
+		output->set(LOW);
+		delete output;
 	}
 
 	// Pulse an output LOW and then return to HIGH after given delay (in milliseconds)
 	// Note this function is blocking
 	inline void pulseLow(byte pin, uint32_t deltaUs = 0) {
-		digitalWrite(pin, LOW);
+		auto output = new DigitalOutput(pin);
+		output->set(LOW);
 		if(deltaUs) delay(deltaUs);
-		digitalWrite(pin, HIGH);
+		output->set(HIGH);
+		delete output;
 	}
 
 	// Pulse an output LOW and then return to HIGH after given delay (in microseconds)
 	// Note this function is blocking
 	inline void pulseLowMicro(byte pin, uint32_t deltaUs = 0) {
-		digitalWrite(pin, LOW);
+		auto output = new DigitalOutput(pin);
+		output->set(LOW);
 		if(deltaUs) delayMicroseconds(deltaUs);
-		digitalWrite(pin, HIGH);
+		output->set(HIGH);
+		delete output;
 	}
 }
 
