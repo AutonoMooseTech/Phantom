d6b7a5cff840884d967811011c089a570e45a718
Liam Kinne <liamkinne@users.noreply.github.com>
2017-09-08 12:42:11 +1000

    Deleted unused typedefs

--- Phantom.h
+++ Phantom.h
@@ -26,26 +26,6 @@ enum {INPUT, OUTPUT, INPUT_PULLUP};
 enum {CHANGE = 1, FALLING = 2, RISING = 3};
 
 namespace Phantom {
-	// Type Definitions
-
-	/*#ifndef int8_t
-	typedef char                 int8_t;
-	typedef short                int16_t;
-	typedef long                 int32_t;
-	typedef long long            int64_t;
-	#endif
-
-	#ifndef uint8_t
-	typedef unsigned char        uint8_t;
-	typedef unsigned short       uint16_t;
-	typedef unsigned long        uint32_t;
-	typedef unsigned long long   uint64_t;
-	#endif
-
-	#ifndef byte
-	typedef unsigned char        byte;
-	#endif */
-
 	// Constants and Defines
 
 	const uint8_t BIT0 = 0x01;	// 00000001
