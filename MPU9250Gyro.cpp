50e183573937868107500c412573db4418a340b1
Liam Kinne <liamkinne@users.noreply.github.com>
2017-09-26 20:20:03 +1000

    clean up

--- MPU9250Gyro.cpp
+++ MPU9250Gyro.cpp
@@ -2,7 +2,10 @@
 
 using namespace Phantom;
 
-MPU9250Gyro::MPU9250Gyro(uint8_t address): MPU9250(address), address(address) {
+MPU9250Gyro::MPU9250Gyro(uint8_t address):
+	MPU9250(address),
+	address(address),
+	scheduler() {
 
 }
 
@@ -13,23 +16,14 @@ void MPU9250Gyro::init() {
 	Wire.write(0x1B);
 	Wire.write(0b00011000);
 	Wire.endTransmission();
-}
-
-void MPU9250Gyro::callibrate() {
+	// Begin Sampling
+	scheduler.add(this, 50);
 }
 
 void MPU9250Gyro::reset() {
 	readings[0] = 0;
 	readings[1] = 0;
 	readings[2] = 0;
-}
-
-int16_t MPU9250Gyro::getRaw(axis_t axis) {
-	switch(axis) {
-		case axis_t::X: return aquire(register_t::GYRO_X); break;
-		case axis_t::Y: return aquire(register_t::GYRO_Y); break;
-		case axis_t::Z: return aquire(register_t::GYRO_Z); break;
-	}
 }
 
 std::initializer_list<int16_t> MPU9250Gyro::get() {
@@ -40,11 +34,11 @@ std::initializer_list<int16_t> MPU9250Gyro::get() {
 	};
 }
 
-int16_t MPU9250Gyro::getX() {
+float MPU9250Gyro::getX() {
 	return readings[axis_t::X];
 }
 
-int16_t MPU9250Gyro::getY() {
+float MPU9250Gyro::getY() {
 	return readings[axis_t::Y];
 }
 
@@ -54,11 +48,23 @@ float MPU9250Gyro::getZ() {
 
 void MPU9250Gyro::update() {
 	uint32_t currentTime = millis();
+
 	for (int i = axis_t::X; i <= axis_t::Z; i++) {
 		int16_t value = getRaw(axis_t(i)) - centers[i];
 		if (fabs(value) >= deadband) {
 			readings[i] += value / -16.4 * (currentTime - timeLast) / 1000;
 		}
 	}
+
 	timeLast = currentTime;
+
+	scheduler.add(this, 50);
+}
+
+int16_t MPU9250Gyro::getRaw(axis_t axis) {
+	switch(axis) {
+		case axis_t::X: return aquire(register_t::GYRO_X); break;
+		case axis_t::Y: return aquire(register_t::GYRO_Y); break;
+		case axis_t::Z: return aquire(register_t::GYRO_Z); break;
+	}
 }
--- MPU9250Gyro.h
+++ MPU9250Gyro.h
@@ -3,11 +3,15 @@
 
 #include "Phantom.h"
 #include "MPU9250.h"
+#include "Scheduler.h"
+#include "SchedulerBase.h"
 #include <initializer_list>
 
 namespace Phantom {
-	class MPU9250Gyro: private MPU9250 {
+	class MPU9250Gyro: private MPU9250, private SchedulerBase {
 	public:
+		Scheduler scheduler;
+
 		MPU9250Gyro(uint8_t address);
 
 		enum axis_t {
@@ -18,26 +22,27 @@ namespace Phantom {
 		
 		void init();
 
-		void callibrate();
-
 		void reset();
-
-		int16_t getRaw(axis_t axis);
 
 		std::initializer_list<int16_t> get();
 
-		int16_t getX();
-		int16_t getY();
+		float getX();
+		float getY();
 		float getZ();
 
-		void update();
+		
 	private:
+		void update();
+
+		int16_t getRaw(axis_t axis);
+
+
 		uint8_t address;
 		
 		int16_t deadband = 50;
 		int16_t centers[3] = {0, 0, -17};
 
-		float readings[3] = {0.0, 0.0, 0.0};
+		float readings[3];
 
 		uint32_t timeLast = millis();
 	};
